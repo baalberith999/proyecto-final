a24b583e3fddbc3eafe9d66cdbe597514a634368
baalberith999 <lucasclemen2001@gmail.com>
2022-12-27 18:23:15 -0300

    Se agregaron componentes NavBar,CartWidget e ItemL

---
+++ src/components/cartwidget/CartWidget.jsx
@@ -0,0 +1,7 @@
+function CartWidget() {
+ return (
+    <a href=""><span><p><img src="https://cdn-icons-png.flaticon.com/512/2438/2438136.png" alt="Carrito"/>0</p></span></a>
+ );
+}
+
+export default CartWidget;
---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,12 @@
+function NavBar(){
+    return (
+    <div>
+        <ul>
+            <li><a href="">Robots Domesticos</a></li>
+            <li><a href="">Robots Serviciales</a></li>
+        </ul>
+    </div>
+    );
+}
+
+export default NavBar;
